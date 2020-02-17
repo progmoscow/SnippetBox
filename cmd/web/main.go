858490e329eb686475072903a67f73ec9cc80647
neon <prog.moscow@gmail.com>
2020-02-17 09:53:46 +0300

    Changing the project structure

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Домашняя страница"))
-}
 
 func main(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet",showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on : 4000")
 	err := http.ListenAndServe(":4000", mux)
